63892bb1a7c2c70ef72e74387d0e9540ed99d5d7
David Björklund <david.bjorklund@gmail.com>
2015-02-08 21:19:39 +0100

    example, license, readme

---
+++ example.js
@@ -0,0 +1,38 @@
+
+var smartCrop = require('./');
+
+var foo = [];
+var opencv = require('opencv');
+var readFileSync = require('fs').readFileSync;
+
+smartCrop({
+  image: readFileSync(__dirname + '/test/obama.jpg'),
+  width: 200,
+  height: 100
+}, function(err, results) {
+    console.log('obama');
+    // results.method === 'faces'
+    console.log(
+      results.method,
+      results.right,
+      results.bottom,
+      results.left,
+      results.top
+    );
+  });
+
+smartCrop({
+  image: readFileSync(__dirname + '/test/moon.jpg'),
+  width: 200,
+  height: 100
+}, function(err, results) {
+    console.log('moon');
+    // results.method === 'good-features'
+    console.log(
+      results.method,
+      results.right,
+      results.bottom,
+      results.left,
+      results.top
+    );
+  });
